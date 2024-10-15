7355443e83af14ae45e94d1cf8838ed55b607514
Manish kumar <40139918+manish774@users.noreply.github.com>
2024-10-15 21:58:42 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use("/profile", profileRouter);
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
   next();
 });
 // User update route
