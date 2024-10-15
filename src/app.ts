6f7bb34ddf0a296f85af64805223ceee91dca44e
Manish kumar <40139918+manish774@users.noreply.github.com>
2024-10-15 20:36:42 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,12 @@ app.use(cors());
 app.use("/auth", authRouter);
 app.use("/profile", profileRouter);
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 // User update route
 app.patch("/userUpdate/:userId", async (req: Request, res: Response) => {
   const userId = req.params.userId;
