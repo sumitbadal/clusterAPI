ff1c4536893a39ca73c8549106003ed4ea8a8894
Manish kumar <40139918+manish774@users.noreply.github.com>
2024-10-19 01:16:37 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin:'https://jalsuvidha.com',
+  origin:'https://manish774.github.io/splitIt/',
   credentials: true
 }));
 
