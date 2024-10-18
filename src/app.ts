dc47b9c7eb78d44e5d3b5be76d67e58483a27f8d
Manish kumar <40139918+manish774@users.noreply.github.com>
2024-10-18 20:08:30 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin:'http://localhost:3000'
+  origin:'http://localhost:3000',
+  credentials: true
 }));
 
 app.use("/auth", authRouter);
