a2616d0d6d811345a9721b4aa05a5eccbde5a4c6
Manish kumar <40139918+manish774@users.noreply.github.com>
2024-10-18 23:20:05 +0530

    l

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin:'http://localhost:3000',
+  origin:'https://jalsuvidha.com/',
   credentials: true
 }));
 
